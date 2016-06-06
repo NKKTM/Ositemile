dd3732c6f462ed5fceb1aae7b9c297c518edf979
hatsune.kitajima <h.kitajima@tcmobile.jp>
2016-06-06 16:22:05 +0900

    ログイン認証更新

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,6 +11,7 @@ import static play.data.Form.*;
 
 import models.entity.*;
 import models.form.*;
+import models.login.*;
 
 public class Application extends Controller {
 
@@ -37,10 +38,10 @@ public class Application extends Controller {
         	//ユーザー情報がフォームから取得できた場合
         	User user = new User();
             user.setUserName(registerForm.get().userName);
-            user.setPassward(registerForm.get().passward);
+            user.setPassword(registerForm.get().password);
             user.setLoginId(registerForm.get().loginId);            
             user.save();
-            System.out.println("DB登録に成功しました！"+registerForm.get().passward);
+            System.out.println("DB登録に成功しました！");
             return redirect("/");            
         }else{
         	//ユーザー情報がフォームから取得できなかった場合
@@ -48,5 +49,38 @@ public class Application extends Controller {
             return redirect("/");             	
         }
     }
+
+    // ログイン認証
+    public static Result authenticate() {
+        Form<LoginForm> loginForm = form(LoginForm.class).bindFromRequest();
+        if (loginForm.hasErrors()) {
+            //ログイン入力がフォームから取得できなかった場合
+            return badRequest(login.render(loginForm));
+        } else {
+            //ログイン入力がフォームから取得できた場合
+            session().clear();
+            session("loginId", loginForm.get().loginId);
+            String returnUrl = ctx().session().get("returnUrl");
+            if(returnUrl == null || returnUrl.equals("") || returnUrl.equals(routes.Application.login().absoluteURL(request()))) {
+                returnUrl = routes.Application.authenticateSuccess().absoluteURL(request());
+            }
+            return redirect(returnUrl);
+        }
+    }
+
+    // 認証成功したらここへ
+    @Security.Authenticated(Secured.class)
+    public static Result authenticateSuccess() {
+            System.out.println("認証成功");
+            return redirect(routes.Application.index().absoluteURL(request()));
+    }
+
+    // ログアウト
+    @Security.Authenticated(Secured.class)
+    public static Result logout() {
+        session().clear();
+        flash("success", "You've been logged out");
+        return redirect(routes.Application.index().absoluteURL(request()));
+    }        
 
 }
